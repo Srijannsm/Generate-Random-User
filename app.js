012092507d60ac7ae96c17e9b8aee855828cdb29
Srijannsm <pradhansrijan07@gmail.com>
2024-02-23 17:20:02 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const app = Vue.createApp({
+  template: "",
+  data() {
+    return {
+      firstName: "John",
+      lastName: "Doe",
+      contact: "+977 9812345678",
+      email: "johnDoe@gmail.com",
+      gender: "male",
+      picture: "https://randomuser.me/api/portraits/men/10.jpg",
+    };
+  },
+  methods: {
+    async getUser() {
+      const res = await fetch("https://randomuser.me/api");
+      const { results } = await res.json();
+
+    //   console.log(results);
+
+      this.firstName = results[0].name.first;
+      this.lastName = results[0].name.last;
+      this.contact = results[0].phone;
+      this.email = results[0].email;
+      this.gender = results[0].gender;
+      this.picture = results[0].picture.large;
+
+    },
+  },
+});
+
+app.mount("#app");
